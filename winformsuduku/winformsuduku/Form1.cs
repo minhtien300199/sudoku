d5c97527f408da2f1e8374543121c56cea24bed3
Vo Minh Hieu <hieundca1@gmail.com>
2018-09-29 23:22:11 +0700

    Them vao winform

--- winformsuduku/winformsuduku/Form1.cs
+++ winformsuduku/winformsuduku/Form1.cs
@@ -1,20 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace winformsuduku
+using System.Diagnostics;
+namespace Timer
 {
-    public partial class Form1 : Form
+    class Program
     {
-        public Form1()
+        static void Main(string[] args)
         {
-            InitializeComponent();
+            Stopwatch st = new Stopwatch();
+            st.Start();
+            long i = 600000000L;
+            Console.WriteLine("Thoi gian thuc hien {0} vong lap rong:", i);
+            while (i > 0)
+             –i;
+            st.Stop();
+            Console.WriteLine("{0} giay", st.Elapsed.ToString());
+            if (Stopwatch.IsHighResolution)
+                Console.WriteLine("Timed with Hi res");
+            else
+                Console.WriteLine("Not Timed with Hi res");
+            Console.ReadKey();
         }
     }
 }

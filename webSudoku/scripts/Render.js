de0ced171a3e0b9f4a475cb5211164199ca7a3e9
minhtien300199 <minhtien300199@gmail.com>
2018-10-25 20:16:14 +0700

    add render.js

---
+++ webSudoku/scripts/Render.js
@@ -0,0 +1,85 @@
+var render = function(boardsArray) {
+    // console.log(boardsArray);
+    var container = document.getElementById("app");
+    var max = 9;
+    // console.log(container);
+    for (var rowIndex = 0; rowIndex < max; rowIndex++) {
+    var div = document.createElement("div");
+    // var addClass=document.querySelector('.col').classList;
+    if ((rowIndex + 1) % 3 === 0) {
+        div.setAttribute("class", "row row-even");
+    } else {
+        if (rowIndex === 0) {
+        div.setAttribute("class", "row row-even-2");
+        } else div.setAttribute("class", "row");
+    }
+
+    for (colIndex = 0; colIndex < max; colIndex++) {
+        var innerDiv = document.createElement("div");
+        var colClass = "col" + cellNumberClass;
+        innerDiv.setAttribute("data-row", rowIndex);
+        innerDiv.setAttribute("data-col", colIndex);
+        if ((colIndex + 1) % 3 === 0) {
+        innerDiv.setAttribute("class", colClass + " col-even");
+        } else {
+        if (colIndex === 0) {
+            innerDiv.setAttribute("class", colClass + " col-even-2");
+        } else {
+            innerDiv.setAttribute("class", colClass);
+        }
+        }
+        try {
+        innerDiv.innerText = boardsArray[rowIndex][colIndex];
+        if (innerDiv.textContent !== "") innerDiv.setAttribute("id", "base");
+        } catch (ex) {
+        console.log(rowIndex, colIndex);
+        }
+        //nhập ma trận. và xử lý nhập xuất số
+        div.appendChild(innerDiv);
+        innerDiv.addEventListener(
+        "click",
+        handleSudokuCellClick(innerDiv, rowIndex, colIndex)
+        );
+    }
+    container.appendChild(div);
+    }
+};
+
+var renderNum = function(max) {
+    var container = document.getElementById("touchpad");
+    for (var index = 0; index < 10; index++) {
+    var innerDiv = document.createElement("div");
+    innerDiv.setAttribute("class", "cell " + cellNumberClass);
+    if (index==max)
+    {
+        innerDiv.innerHTML="Del";
+    } else
+    {
+        innerDiv.innerHTML = index + 1;
+    }
+    innerDiv.addEventListener(
+        "click",
+        handleTouchPadCellClick(innerDiv, index)
+    );
+    container.appendChild(innerDiv);
+    }
+};
+
+var renderDrafts=function(max)         //hàm dựng viết nháp.
+{
+var container=document.getElementById("drafts");
+for (var index=0;index<10;index++)
+{
+    var innderDiv=document.createElement("div");
+    innderDiv.setAttribute("class","draft-num");
+    if (index===max)
+    {
+    innderDiv.innerHTML="Del";
+    }
+    else
+    {
+    innderDiv.innerHTML=index+1;
+    }
+    container.appendChild(innderDiv);
+}
+};
--- webSudoku/scripts/index.js
+++ webSudoku/scripts/index.js
@@ -5,10 +5,6 @@ var resetButton= function()
   {
     undoButton();
   }
-  // while (redoStack.length!=0)
-  // {
-  //   redoButton();
-  // }
   undoStack=[];
   undoSelectedCell=[];
   redoStack=[];
@@ -216,73 +212,7 @@ var handleSudokuCellClick = function(innerDiv, rowIndex, colIndex) {
   };
 };
 
-var renderNum = function(max) {
-  var container = document.getElementById("touchpad");
-  for (var index = 0; index < 10; index++) {
-    var innerDiv = document.createElement("div");
-    innerDiv.setAttribute("class", "cell " + cellNumberClass);
-    if (index==max)
-    {
-      innerDiv.innerHTML="Del";
-    } else
-    {
-      innerDiv.innerHTML = index + 1;
-    }
-    innerDiv.addEventListener(
-      "click",
-      handleTouchPadCellClick(innerDiv, index)
-    );
-    container.appendChild(innerDiv);
-    //innerDiv.getElementsByClassName('numSet').addEventListener('Click',);
-  }
-};
 
-var render = function(boardsArray) {
-  // console.log(boardsArray);
-  var container = document.getElementById("app");
-  var max = 9;
-  // console.log(container);
-  for (var rowIndex = 0; rowIndex < max; rowIndex++) {
-    var div = document.createElement("div");
-    // var addClass=document.querySelector('.col').classList;
-    if ((rowIndex + 1) % 3 === 0) {
-      div.setAttribute("class", "row row-even");
-    } else {
-      if (rowIndex === 0) {
-        div.setAttribute("class", "row row-even-2");
-      } else div.setAttribute("class", "row");
-    }
-
-    for (colIndex = 0; colIndex < max; colIndex++) {
-      var innerDiv = document.createElement("div");
-      var colClass = "col" + cellNumberClass;
-      innerDiv.setAttribute("data-row", rowIndex);
-      innerDiv.setAttribute("data-col", colIndex);
-      if ((colIndex + 1) % 3 === 0) {
-        innerDiv.setAttribute("class", colClass + " col-even");
-      } else {
-        if (colIndex === 0) {
-          innerDiv.setAttribute("class", colClass + " col-even-2");
-        } else {
-          innerDiv.setAttribute("class", colClass);
-        }
-      }
-      try {
-        innerDiv.innerText = boardsArray[rowIndex][colIndex];
-        if (innerDiv.textContent !== "") innerDiv.setAttribute("id", "base");
-      } catch (ex) {
-        console.log(rowIndex, colIndex);
-      }
-      //nhập ma trận. và xử lý nhập xuất số
-      div.appendChild(innerDiv);
-      innerDiv.addEventListener(
-        "click",
-        handleSudokuCellClick(innerDiv, rowIndex, colIndex)
-      );
-    }
-    container.appendChild(div);
-  }
-};
 var t = new Date();
 var timer = 0;
 var timerInterval = null;
@@ -334,6 +264,7 @@ document.addEventListener("DOMContentLoaded", function() {
   copyOfSudokuArr();
   render(sudokuArray);
   renderNum(max);
+  renderDrafts(max);
 });
 
 document.addEventListener("click", function(event) {

dcacae00a963105ec855559b4f28fea790bc6f79
minhtien300199 <minhtien300199@gmail.com>
2018-12-13 21:39:18 +0700

    changed coded

--- webSudoku/scripts/generator.js
+++ webSudoku/scripts/generator.js
@@ -1,12 +1,9 @@
 // console.log(Math.floor(Math.random()*10+1))
 // hàm này chạy từ 1 tới 9
-var RanNum = function () {
+var randomize = function () {
   // từ 1 tới 10
-  var num = Math.floor(Math.random() * 10 + 1);
-  if (num < 10) {
-    return num; // từ 1 tới 9
-  }
-  return num - 1; // lớn hơn 10 thì return 9.
+  var num = Math.floor(Math.random() * 10 + 1);        
+  return (num < 10 ? num : 9); // từ 1 tới 9          
 };
 
 
@@ -16,18 +13,20 @@ var matrixGenerator = function () {
   var delrow;
   // Example : level easy is 42
   var blank = 81 - level;
-  sudokuArray[4][4] = RanNum();
-  sudokuArray[0][0] = RanNum();
-  sudokuArray[8][8] = RanNum();
-  sudokuArray[2][7] = RanNum();
-  sudokuArray[7][2] = RanNum();
+  // console.log(sudokuArray);
+  sudokuArray[4][4] = randomize();
+  sudokuArray[0][0] = randomize();
+  sudokuArray[8][8] = randomize();
+  sudokuArray[2][7] = randomize();
+  sudokuArray[7][2] = randomize();
+  // console.log(sudokuArray);
   copyOfSudokuArr();  //chuyển sudokuarr qua solvesudokuarr để xử lý
   SolveSu();
   //matrix = solveSudokuArr;
   while (index <= blank) {
     try {
-      delcol = RanNum() - 1;
-      delrow = RanNum() - 1;
+      delcol = randomize() - 1;
+      delrow = randomize() - 1;
       if (solveSudokuArr[delrow][delcol] != '')
       {
         solveSudokuArr[delrow][delcol] = '';
